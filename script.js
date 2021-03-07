7e54f1bc1afb5a36be4c1e627eb0e4a398186948
Ramiro <rampare99@gmail.com>
2021-03-07 17:34:28 -0300

    Added basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('working')
+const container = document.querySelector("#container");
+
+const square = document.createElement("div");
+square.classList.add("square");
+
+const createSquares = (numberOfSquares) => {
+  container.appendChild(square);
+  for (let i = 1; i < numberOfSquares * numberOfSquares; i++) {
+    const squareDup = square.cloneNode(true);
+    container.appendChild(squareDup);
+  }
+};
+
+createSquares(16);
+
+const clearButton = document.querySelector("#clear-button");
+
+clearButton.addEventListener("click", () => {
+  const newNumberOfSquares = prompt("Please choose the number of squares");
+  if (newNumberOfSquares) {
+      if (newNumberOfSquares > 100 || newNumberOfSquares < 1) {
+          alert('Please choose a number between 1 and 100.');
+          return;
+      }
+    container.innerHTML = "";
+    createSquares(newNumberOfSquares);
+    document.documentElement.style.setProperty(
+      "--squareNum",
+      newNumberOfSquares
+    );
+  }
+});
